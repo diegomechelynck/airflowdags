5c9268b733f6b34c7a4f21f19080a8e97741042e
Diego Mechelynck <diego@agilityc.be>
2021-05-23 10:10:48 +0200

    push the dag

---
+++ Databricksdag.py
@@ -0,0 +1,33 @@
+from airflow import DAG
+from airflow.providers.databricks.operators.databricks import DatabricksSubmitRunOperator, DatabricksRunNowOperator
+from datetime import datetime, timedelta 
+
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=2)
+}
+
+with DAG('databricks_dag2',
+    start_date=datetime(2021, 1, 1),
+    schedule_interval='@daily',
+    catchup=False,
+    default_args=default_args
+    ) as dag:
+    helloworld1 = DatabricksRunNowOperator(
+        task_id='run_now',
+        databricks_conn_id='Databricksdieg',
+        job_id=5
+    )
+
+    helloworld2 = DatabricksRunNowOperator(
+        task_id='run_now2',
+        databricks_conn_id='Databricksdieg',
+        job_id=5
+    )
+
+    helloworld1 >> helloworld2
